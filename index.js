461a17b61269955f6887f2cb408f590b34defc1f
Carry kim <66009926+gimseonjin@users.noreply.github.com>
2020-08-19 01:55:16 +0900

    Create gitignore

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hi');
+const express = require('express')
+const app = express()
+
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening on : http://localhost:${PORT}`);
+}
+
+function handleHome(req, res){
+    res.send('hello world');
+}
+
+function handleProfile(req, res){
+    res.send("you are on my profile")
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT,handleListening);
